6d9debfa1fc04c1eaae6926a4c87d26788614be4
WatabeYujin <30500246+WatabeYujin@users.noreply.github.com>
2019-02-19 19:10:44 +0900

    ノード効果、跳弾・貫通の実装

--- Scripts/Bullet/StickWeponBullet.cs
+++ Scripts/Bullet/StickWeponBullet.cs
@@ -9,17 +9,26 @@ public class StickWeponBullet : ObjectPool
     [SerializeField]
     private TrailRenderer trailRenderer;            //弾のTrailRenderer
     [SerializeField]
+    private Collider thisCollider;
+    [SerializeField]
     private string targetTagName = "Enemy";         //敵のtag名
     [SerializeField]
     private string shooterTagName = "Player";       //プレイヤーのtag名  
     [SerializeField]
     private GameObject hitEffect;                   //命中時のエフェクト
-
+    [SerializeField]
+    private PhysicMaterial bounceMat;
+    
 
     private Status.StraightWeponStatus straightWeponStatus;
     private Vector3 bulletSpawnPosition;            //弾の発射地点
     private bool isDestroy = false;                 //弾が消失するモードに入っているか
-    private Collider hitCollider;                   //命中した対象のCollider
+    private Transform hitTransform;                  //命中した対象のTransform
+    private const float baseBulletSpeed = 0.1f;
+    private SaveScriptableObject2 save;
+    private int bounceCount = 0;
+    private int throughCount = 0;
+    private float damageBoost = 1f;
 
     private void OnEnable()
     {
@@ -38,13 +47,17 @@ public class StickWeponBullet : ObjectPool
     /// 弾の移動を開始させる処理
     /// </summary>
     void StartBulletMove() {
-        const float m_baseBulletSpeed = 100;
-
+        bounceCount = save.straightNode[0].GetLevel;
+        throughCount = save.straightNode[1].GetLevel;
+        if (throughCount >= 1)
+            thisCollider.isTrigger = true;
+        else if (bounceCount >= 1)
+            thisCollider.material = bounceMat;
         if (thisRigidbody == null) return;
-
+        thisRigidbody.isKinematic = false;
         thisRigidbody.AddForce(
-            transform.forward * straightWeponStatus.bulletSpeed * m_baseBulletSpeed +
-            transform.right * Recoil()  * m_baseBulletSpeed /2
+            transform.forward * straightWeponStatus.bulletSpeed * baseBulletSpeed +
+            transform.right * Recoil()  * baseBulletSpeed / 2
         );
     }
 
@@ -53,8 +66,12 @@ public class StickWeponBullet : ObjectPool
     /// </summary>
     /// <returns>ブレの大きさ（float）を返す</returns>
     float Recoil() {
+        float m_recoil = 0;
+        const float m_baseDiminution = 2f;
         if (straightWeponStatus.recoil == 0) return 0;
-        return Random.Range(-straightWeponStatus.recoil, straightWeponStatus.recoil)/2f ;
+        m_recoil = Random.Range(-straightWeponStatus.recoil, straightWeponStatus.recoil);
+        m_recoil /= (m_baseDiminution + save.straightNode[10].GetLevel);
+        return m_recoil;
     }
     
     void Update() {
@@ -80,6 +97,7 @@ public class StickWeponBullet : ObjectPool
     /// </summary>
     IEnumerator BulletDelete()
     {
+        thisRigidbody.isKinematic = true;
         isDestroy = true;
         thisRigidbody.velocity = Vector3.zero;
         float m_deleteTime = 0;
@@ -93,33 +111,102 @@ public class StickWeponBullet : ObjectPool
     {
         Transform m_hitEffect = Objectspawn(hitEffect, transform.position, transform.rotation).transform;
     }
+    
+    void OnCollisionEnter(Collision col) {
+        hitTransform = col.transform;
+        HitEvent();
+    }
 
-    void OnTriggerEnter(Collider col) {
-        hitCollider = col;
-        if(!isDestroy)
+    void OnTriggerEnter(Collider col)
+    {
+        hitTransform = col.transform;
+        HitEvent();
+    }
+
+    void HitEvent()
+    {
+        if (!isDestroy)
             TargetTagCheck();
+        switch (HitActionCheck())
+        {
+            case 2:
+                //貫通のパターン
+                ThroughEffect();
+                break;
+            case 1:
+                //跳弾のパターン
+                BounceEffect();
+                break;
+            default:
+                //何もないパターン
+                StartCoroutine(BulletDelete());
+                break;
+        }
+    }
+    
+
+    /// <summary>
+    /// 命中時の弾丸の効果を調べる
+    /// </summary>
+    /// <returns></returns>
+    int HitActionCheck()
+    {
+        int m_value=0;
+
+        //貫通回数の確認
+        if (throughCount > 0)
+            m_value = 2;
+        //跳弾回数の確認
+        else if (bounceCount > 0)
+            m_value = 1;
+        return m_value;
+    }
+    
+    //貫通効果付加時の処理
+    void ThroughEffect() {
+        damageBoost += 0.2f;
+        throughCount--;
+        //貫通回数がなくなった場合
+        if (throughCount <= 0)
+        {
+            thisCollider.isTrigger = false;
+            //もし跳弾が可能だった場合、マテリアルを適用する。
+            if(bounceCount > 0)
+                thisCollider.material = bounceMat;
+        }
+    }
+
+    //跳弾効果付加時の処理
+    void BounceEffect() {
+        bounceCount--;
+        //跳弾回数がなくなった場合、マテリアルを外す
+        if (bounceCount <= 0)
+            thisCollider.material = null;
     }
 
     /// <summary>
     /// 命中相手のtagが条件に当てはまるか確かめる
     /// </summary>
     void TargetTagCheck() {
-        if (hitCollider.tag == "Event") return;
-        if (hitCollider.tag == shooterTagName) return;
-        if (hitCollider.tag == this.tag) return;
-        if (hitCollider.tag == targetTagName) HitEvent();
+        if (hitTransform.tag == "Event") return;
+        if (hitTransform.tag == shooterTagName) return;
+        if (hitTransform.tag == this.tag) return;
+        if (hitTransform.tag == targetTagName) DamgeEvent();
         EffectSpawn();
-        StartCoroutine(BulletDelete());
     }
 
     /// <summary>
     /// 命中時の処理
     /// </summary>
-    void HitEvent() {
-        hitCollider.GetComponent<Life>().Damage(straightWeponStatus.GetDamage());
+    void DamgeEvent() {
+        float m_lastDamage = straightWeponStatus.GetDamage() * damageBoost;
+        hitTransform.GetComponent<Life>().Damage((int)m_lastDamage);
     }
+
     public void SetStatus(Status.StraightWeponStatus value)
     {
+        if (save == null)
+            save = (SaveScriptableObject2)Resources.Load("SaveData");
         straightWeponStatus = value;
         SpawnPositionSet();
         StartBulletMove();
